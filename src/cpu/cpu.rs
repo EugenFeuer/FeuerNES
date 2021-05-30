5d18967ae761ddb765bbf425ad3277d1e6034126
EugenFeuer <eugenfeuerfeuer@gmail.com>
2021-05-31 03:21:40 +0800

    feat(AddressMode): add AddressMode, LDA impl AddressMode op

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -3,6 +3,19 @@ const RESET_INTERRUPT_MEM_LOC: u16 = 0xFFFC;
 
 const MEMORY_CAP: usize = 0xFFFF;
 
+#[derive(Debug)]
+enum AddressMode {
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+    NoneAddressing
+}
 
 pub struct Memory {
     raw_mem: [u8; MEMORY_CAP]
@@ -65,6 +78,53 @@ impl CPU {
         }
     }
 
+    fn get_operand_address(&self, mode: &AddressMode) -> u16 {
+        match mode {
+            AddressMode::Immediate => {
+                self.pc
+            }
+            AddressMode::ZeroPage => {
+                self.mem.read(self.pc) as u16
+            }
+            AddressMode::ZeroPageX => {
+                let pos = self.mem.read(self.pc);
+                pos.wrapping_add(self.rx) as u16
+            }
+            AddressMode::ZeroPageY => {
+                let pos = self.mem.read(self.pc);
+                pos.wrapping_add(self.ry) as u16
+            }
+            AddressMode::Absolute => {
+                self.mem.read_u16(self.pc)
+            }
+            AddressMode::AbsoluteX => {
+                let pos = self.mem.read_u16(self.pc);
+                pos.wrapping_add(self.rx as u16) as u16
+            }
+            AddressMode::AbsoluteY => {
+                let pos = self.mem.read_u16(self.pc);
+                pos.wrapping_add(self.ry as u16) as u16
+            }
+            AddressMode::IndirectX => {
+                let base = self.mem.read(self.pc);
+                let ptr = (base as u8).wrapping_add(self.rx) as u8;
+                let lo = self.mem.read(ptr as u16);
+                let hi = self.mem.read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            AddressMode::IndirectY => {
+                let base = self.mem.read(self.pc);
+                let lo = self.mem.read(base as u16);
+                let hi = self.mem.read(base.wrapping_add(1) as u16);
+                let deref_base = (hi as u16) << 8 | (lo as u16);
+                deref_base.wrapping_add(self.ry as u16)
+            }
+            AddressMode::NoneAddressing => {
+                panic!("not support for {:?}", mode)
+            }
+        }
+    }
+
     fn update_zero_flag(&mut self, flag: u8) {
         if flag == 0 {
             self.status |= 0b0000_0010
@@ -81,7 +141,10 @@ impl CPU {
         }
     }
 
-    fn lda(&mut self, value: u8) {
+    fn lda(&mut self, mode: &AddressMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem.read(addr);
+
         self.acc = value;
         self.update_neg_flag(value);
         self.update_zero_flag(value);
@@ -120,11 +183,40 @@ impl CPU {
                 0x00 => {
                     return;
                 }
-                0x9A => {
-                    let value = self.mem.read(self.pc);
+                // begin of LDA
+                0xA9 => {
+                    self.lda(&AddressMode::Immediate);
                     self.pc += 1;
-                    self.lda(value);
                 }
+                0xA5 => {
+                    self.lda(&AddressMode::ZeroPage);
+                    self.pc += 1;
+                }
+                0xB5 => {
+                    self.lda(&AddressMode::ZeroPageX);
+                    self.pc += 1;
+                }
+                0xAD => {
+                    self.lda(&AddressMode::Absolute);
+                    self.pc += 1;
+                }
+                0xBD => {
+                    self.lda(&AddressMode::AbsoluteX);
+                    self.pc += 1;
+                }
+                0xB9 => {
+                    self.lda(&AddressMode::AbsoluteY);
+                    self.pc += 1;
+                }
+                0xA1 => {
+                    self.lda(&AddressMode::IndirectX);
+                    self.pc += 1;
+                }
+                0xB1 => {
+                    self.lda(&AddressMode::IndirectY);
+                    self.pc += 1;
+                }
+                // end of LDA
                 0xAA => {
                     self.pc += 1;
                     self.tax();
