b305daa5666435961c32ce636fc570ef216cbec8
EugenFeuer <eugenfeuerfeuer@gmail.com>
2021-06-01 23:12:20 +0800

    feat(cpu): add load_program fn

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -243,6 +243,25 @@ impl CPU {
         self.mem.write_u16(RESET_INTERRUPT_MEM_LOC, PROGRAM_BEGIN_LOC);
     }
 
+    pub fn load_program(&mut self, program: Vec<u8>) {
+        let ram = [0u8; 0x2000];          // RAM [0x0000..0x2000]
+        let io = [0u8; 0x2020];           // IO [0x2000..0x4020]
+        let special = [0u8; 0x1FE0];      // mappers - special circuitry [0x4020..0x6000]
+        let reserved = [0u8; 0x2000];     // reserved to a RAM space [0x6000..0x8000]
+    
+        // program [0x8000..0x10000]
+    
+        let mut bytes = [0u8; 0xFFFF];
+        bytes[0x0000..0x2000].copy_from_slice(&ram[..]);
+        bytes[0x2000..0x4020].copy_from_slice(&io[..]);
+        bytes[0x4020..0x6000].copy_from_slice(&special[..]);
+        bytes[0x6000..0x8000].copy_from_slice(&reserved[..]);
+        bytes[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+        bytes[0x8000..(0x8000 + program.len())].copy_from_slice(&program);
+        
+        self.load(bytes);
+    }
+
     pub fn reset(&mut self) {
         self.acc = 0;
         self.rx = 0;
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,11 @@ extern crate lazy_static;
 fn main() {
     let mut cpu = cpu::CPU::new();
     
-    let ram = [0u8; 0x2000];          // RAM [0x0000..0x2000]
-    let io = [0u8; 0x2020];           // IO [0x2000..0x4020]
-    let special = [0u8; 0x1FE0];      // mappers - special circuitry [0x4020..0x6000]
-    let reserved = [0u8; 0x2000];     // reserved to a RAM space [0x6000..0x8000]
-
-    // program [0x8000..0x10000]
     let program: Vec<u8> = vec!(
-        0xA9, 0x20, 0x0
+        0x69, 0xD0, 0x69, 0x90, 0x00
     );
 
-    let mut bytes = [0u8; 0xFFFF];
-    bytes[0x0000..0x2000].copy_from_slice(&ram[..]);
-    bytes[0x2000..0x4020].copy_from_slice(&io[..]);
-    bytes[0x4020..0x6000].copy_from_slice(&special[..]);
-    bytes[0x6000..0x8000].copy_from_slice(&reserved[..]);
-    bytes[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
-
-    cpu.load(bytes);
+    cpu.load_program(program);
     cpu.run();
 
     println!("Hello, NES!");
