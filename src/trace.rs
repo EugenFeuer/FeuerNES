40480a040e555d53490e725766cc4f5ca5c03a04
EugenFeuer <eugenfeuerfeuer@gmail.com>
2021-08-19 03:00:55 +0800

    fix(trace): remove debug print

--- src/trace.rs
+++ src/trace.rs
@@ -17,12 +17,12 @@ pub struct TraceInfo {
 }
 
 impl TraceInfo {
-    pub fn new(frame: u32, cpu: &cpu::CPU) -> Self {
+    pub fn new(frame: u32, cpu: &mut cpu::CPU) -> Self {
         let ref opcodes: HashMap<u8, &'static opcode::Opcode> = *opcode::OPCODES_MAP;
         let op = cpu.mem_read(cpu.pc);
         let opcode = opcodes
             .get(&op)
-            .expect(&format!("op: {:x} not exists or not impl.", op));
+            .expect(&format!("op: {:x} not exists or not impl .", op));
         TraceInfo {
             frame: frame,
             pc: cpu.pc,
@@ -43,7 +43,7 @@ impl TraceInfo {
     }
 }
 
-pub fn trace(cpu: &cpu::CPU, frame: &u32) {
+pub fn trace(cpu: &mut cpu::CPU, frame: &u32) {
     println!("========== FRAME: {} ==========", frame);
 
     let _pc = cpu.pc;
@@ -62,23 +62,6 @@ pub fn trace(cpu: &cpu::CPU, frame: &u32) {
     use web_sys::console;
     // console::log_1(&format!("frame: {}", trace_info.dump()).into());
     // println!("{}", trace_info.dump());
-
-    println!("PC: {:#02X}, INSTRUCT: {}", _pc, instruction.name);
-    println!(
-        "SP: {:#02X}, VALUE: {:#02X}",
-        cpu.sp,
-        cpu.mem_read(cpu.sp as u16 + 0x100)
-    );
-    println!("ACC: {:#02X}", cpu.acc);
-    println!("RX: {:#02X}", cpu.rx);
-    println!("RY: {:#02X}", cpu.ry);
-    println!("STATUS: {:?}", cpu.status);
-    println!("");
-
-    println!(
-        "mode: {:?}, addr: {:#02X}, value: {:#02X}",
-        instruction.mode, addr, value
-    );
 
     match instruction.mode {
         AddressMode::Immediate => {}
