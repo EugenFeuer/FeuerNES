597d06c625ef90708d0c49dd8234743ea323adb6
EugenFeuer <eugenfeuerfeuer@gmail.com>
2021-08-19 03:03:20 +0800

    feat(ppu): impl for read and write to ppu, and get mirror vram address

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,6 +1,6 @@
 ﻿use crate::cartridge::MirroringType;
 
-mod registers;
+pub mod registers;
 use self::registers::controller::*;
 use self::registers::mask::*;
 use self::registers::status::*;
@@ -36,9 +36,11 @@ pub struct PPU{
     pub scroll_register: PPUSCROLL,
     pub address_register: PPUADDR,
     pub data_register: PPUDATA,
+
+    _internal_last_read_byte: u8
 }
 
-impl PPU {
+impl PPU{
     pub fn new(chr: Vec<u8>, mirroring_type: MirroringType) -> Self {
         PPU {
             chr: chr,
@@ -55,6 +57,58 @@ impl PPU {
             scroll_register: PPUSCROLL::new(),
             address_register: PPUADDR::new(),
             data_register: PPUDATA::new(),
+
+            _internal_last_read_byte: 0
+        }
+    }
+
+    pub fn read(&mut self) -> u8 {
+        let addr = self.address_register.get_address();
+        self.address_register.increment_address(self.ctrl_register.get_vram_address_increment());
+
+        match addr {
+            0x0000..=0x1FFF => {
+                self._internal_last_read_byte = self.chr[addr as usize];
+                self._internal_last_read_byte
+            }
+            0x2000..=0x2FFF => {
+                self._internal_last_read_byte = self.vram[(addr - 0x2000) as usize];
+                self._internal_last_read_byte
+            }
+            0x3000..=0x3EFF => panic!("not used"),
+            0x3F00..=0x3FFF => self.palette[(addr - 0x3F00) as usize],
+            _ => panic!("unexpected address access: {:x}", addr)
+        }
+    }
+
+    pub fn write(&mut self, data: u8) {
+        let addr = self.address_register.get_address();
+        self.address_register.increment_address(self.ctrl_register.get_vram_address_increment());
+
+        match addr {
+            0x0000..=0x1FFF => panic!("writing to chr rom {:x}", addr),
+            0x2000..=0x2FFF => self.vram[(addr - 0x2000) as usize] = data,
+            0x3000..=0x3EFF => panic!("not used"),
+            // mirrors of $3F00/$3F04/$3F08/$3F0C
+            0x3F10 | 0x3F14 | 0x3F18 | 0x3F1C => {
+                let add_mirror = addr - 0x10;
+                self.palette[(addr - 0x10 - 0x3F00) as usize] = data;
+            }
+            0x3F00..=0x3FFF => self.palette[(addr - 0x3F00) as usize] = data,
+            _ => panic!("unexpected address access: {:x}", addr)
+        }
+    }
+
+    pub fn get_mirror_vram_addr(&self, mut addr: u16) -> u16 {
+        addr &= 0x2FFF; // 0x3000-0x3FFF -> 0x2000-0x2FFF (0x3F00-0x3FFF should not pass in)
+        addr -= 0x2000; // 0x2000-0x2FFF -> 0x0000-0x0FFF
+        let index = addr / 0x400;  // 0x0000-0x0FFF -> 0-3 screen index
+        match (&self.mirroring_type, index) {
+            (MirroringType::Vertical, 2) | (MirroringType::Vertical, 3) => addr - 0x800,    // 0x400-0x800
+            (MirroringType::Horizontal, 1) => addr - 0x400,     // 0-0x400
+            (MirroringType::Horizontal, 2) => addr - 0x400,     // 0x400-0x800
+            (MirroringType::Horizontal, 3) => addr - 0x800,     // 0x400-0x800
+            _ => addr   // no need to map
         }
     }
 }
